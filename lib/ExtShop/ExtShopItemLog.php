f47637b19d56baea00f9381b8a3a799d987c5cda
Bae Jeff <gug@kakao.com>
2024-10-17 14:17:49 +0900

    edit json response

--- lib/ExtShop/ExtShopItemLog.php
+++ lib/ExtShop/ExtShopItemLog.php
@@ -157,7 +157,12 @@ class ExtShopItemLog {
             ])
             ->from('ext_shop_item_log')
             ->getOne();
-        $logOrders = $extItemOrder->getLogOrders(['parent_uuid' => $params['uuid']]);
+        $logOrders = $this->db->select(['*'])
+            ->from('ext_shop_item_orders')
+            ->where([
+                'ex_order_parent' => $params['uuid']
+            ])
+            ->get();
         $totalUseDays = 0;
         $totalDownDays = 0;
         $today = date('Ymd');
@@ -191,10 +196,14 @@ class ExtShopItemLog {
         }
 
         return $this->response([
-            'remainUseDays' => $remainUseDays <= 1 ? 0: $remainUseDays,
-            'remainDownDays' => $remainDownDays <= 1 ? 0: $remainDownDays,
-            'useEndDate' => $expirationUseDate,
-            'downEndDate' => $expirationDownDate
+            'useStatus' => [
+                'days' => $remainUseDays <= 1 ? 0: $remainUseDays,
+                'date' => $expirationUseDate
+            ],
+            'downloadStatus' => [
+                'days' => $remainDownDays <= 1 ? 0: $remainDownDays,
+                'date' => $expirationDownDate
+            ]
         ]);
     }
     public function update($data, $conditions) {
@@ -206,71 +215,6 @@ class ExtShopItemLog {
             'log_status' => 'D',
             'deleted_date' => date('Y-m-d H:i:s')
         ],$conditions);
-    }
-    public function refreshExpiredLogs($memberId) {
-        // 오늘 날짜
-        $today = date('Ymd');
-
-        // Active 상태인 로그를 가져옵니다.
-        $activeLogs = $this->db->select(['*'])
-            ->from('ext_shop_item_log')
-            ->where([
-                'member_id' => $memberId,
-                'log_status' => 'A'
-            ])
-            ->get();
-
-        foreach ($activeLogs as $log) {
-            // 관련된 주문을 가져옵니다.
-            $orders = $this->db->select(['item_use_days', 'item_option_full'])
-                ->from('ext_shop_item_orders')
-                ->where(['member_id' => $memberId, 'item_option_full' => $log['item_option']])
-                ->get();
-
-            // 사용 가능한 일수의 합을 계산합니다.
-            $totalUseDays = 0;
-            foreach ($orders as $order) {
-                $totalUseDays += $order['item_use_days'];
-            }
-
-            // start_date와 totalUseDays를 비교하여 만료 체크
-            $startDate = $log['start_date'];
-            $expirationDate = date('Ymd', strtotime($startDate . " + $totalUseDays days"));
-
-            // 오늘 날짜와 만료 날짜를 비교하여 남은 일수를 계산합니다.
-            $today = new DateTime($today);
-            $expirationDateObj = new DateTime($expirationDate);
-            $interval = $today->diff($expirationDateObj);
-            $remainDays = $interval->days; // 남은 일수
-
-            if ($expirationDate < $today) {
-                // 만료된 로그를 "End"로 업데이트
-                $this->db->update('ext_shop_item_log', ['log_status' => 'E'], ['id' => $log['id']]);
-                $newLogUUid =  mekeUuid();
-                // 새로운 로그를 추가합니다.
-                $newLogData = [
-                    'uuid' => $newLogUUid, // 새로운 UUID 생성
-                    'item_id' => $log['item_id'], // 기존 로그의 item_id 사용
-                    'item_option' => $log['item_option'],
-                    'member_id' => $memberId,
-                    'start_date' => $today, // 오늘 날짜로 시작
-                    'end_date' => null, // 종료일은 null로 설정
-                    'log_status' => 'A',
-                    'remain_download_days' => null, // 필요 시 설정
-                    'remain_use_days' => null, // 필요 시 설정
-                    'creater' => $log['creater']
-                ];
-
-                $rsInsert = $this->db->insert('ext_shop_item_log', $newLogData);
-                // 결과 배열 구성
-                $result = array(
-                    'status' => $rsInsert,
-                    'logUUid' => $newLogUUid, // UUID를 새로 생성한 로그에서 가져옵니다.
-                    'remainDays' => ($expirationDate < $today->format('Ymd')) ? 0 : $remainDays // 만약 만료일이 오늘 이전이라면 0으로 설정
-                );
-            }
-        }
-        return $result;
     }
 }
 
